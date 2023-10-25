c86ec0a62ac5a4887a5a65363d27a90663cf4969
newtonah0 <ezmh015@gmail.com>
2023-10-25 17:16:18 +0300

    fix schema file

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id intger not null,
+    id intger NOT NULL,
     name varchar(60),
     date_of_birth date,
     escape_attempts integer,
