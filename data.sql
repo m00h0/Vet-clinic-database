cbdf9cbd31e37c9e6c2520de61e562f6b6c6276a
newtonah0 <ezmh015@gmail.com>
2023-10-25 16:50:32 +0300

    fix GitHub flow

--- data.sql
+++ data.sql
@@ -1,8 +1,1 @@
-/* Populate database with sample data. */
-
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg ) VALUES
- (1, 'Agumon' ,'Feb 3 2020', 0, true, 10.23),
- (2, 'Gabumon' ,'Nov 15 2018', 8, true, 8),
- (3, 'Pikachu' ,'Jan 7 2021', 1, false,  15.04),
- (4, 'Devimon' ,'May 12 2017', 5, true, 11);
 
